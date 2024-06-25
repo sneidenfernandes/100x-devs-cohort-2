93059d143fd3c4b9a2ce00f3c5946a075e200516
sneidenfernandes <sneiden@gmail.com>
2024-06-25 23:20:09 +0530

    week-2 assignments complete

--- full-stack/assignments/week-2/02-nodejs/fileServer.js
+++ full-stack/assignments/week-2/02-nodejs/fileServer.js
@@ -18,4 +18,32 @@ const path = require('path');
 const app = express();
 
 
+app.get('/files', (req, res) =>{
+  fs.readdir(path.join(__dirname, './files'), (err,files) => {
+    if (err){
+      return res.status(500).json({error: 'Failed to retrieve files.'})
+    }else{
+      res.status(200).json(files);
+    }
+    
+  })
+  
+});
+
+
+app.get('/file/:filename', (req, res) => {
+  const filePath = path.join(__dirname, './files/', req.params.filename);
+
+  fs.readFile(filePath, 'utf-8', (err,data) => {
+    if(err) {
+      return res.status(404).send('File not found');
+    }
+    res.send(data);
+  });
+});
+
+app.all('*', (req, res) => {
+  res.status(404).send('Route not found');
+});
+
 module.exports = app;
