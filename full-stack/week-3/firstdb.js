41d37d46e6f73e5ca3ccc51800e40c5948d3d0e8
sneidenfernandes <sneiden@gmail.com>
2024-07-08 22:57:15 +0530

    more databases

--- full-stack/week-3/firstdb.js
+++ full-stack/week-3/firstdb.js
@@ -1,10 +1,33 @@
 const mongoose = require("mongoose");
+const express = require("express");
+const app = express();
 
-mongoose.connect("mongodb+srv://sneiden:kp7AC0458KcNRRWJ@cluster0.l4godw5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+app.use(express.json());
+mongoose.connect("mongodb+srv://sneiden:uXoCu1i69OkbLSKk@cluster0.l4godw5.mongodb.net/user_app?retryWrites=true&w=majority&appName=Cluster0")
+
+const User =  mongoose.model('Users', {name: String, email:String, password: String});
 
 
-const User = mongoose.model('Users', {name: String, email: String, password: String});
+app.get('/signup', async function(req,res){
+    const username = req.username;
+    const name = req.name;
+    const password = req.password;
 
+    const existingUser = await User.findone({email: username});
 
-const user = new User({name: "Harkirat Singh", email: "something@gmail.com", password: "123456"});
-user.save();
+    if(exisitingUser){
+        return res.status(400).send("User already exists!");
+    }
+    
+
+    const user  = new User({
+        name: name,
+        email: username,
+        password: password
+    });
+
+    user.save();
+    json.send({
+        "msg": "User successfully created."
+    });
+});
